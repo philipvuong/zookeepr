9d31cd9c977760feb30834912da1d128ebff5b2c
Philip Vuong <40782386+philipvuong@users.noreply.github.com>
2020-09-09 16:24:13 -0500

    findById

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { animals } = require('./data/animals');
 
 const express = require('express');
 
+const PORT = process.env.PORT || 3001
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -38,6 +40,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -46,6 +53,15 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
